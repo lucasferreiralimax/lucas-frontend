1995f60326e1df3f9923f77be26fe863a3e68716
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-09-29 22:35:03 -0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className='app'>
+      <h1 className='name'>Lucas Ferreira de Lima</h1>
+      <p>Desenvolvedor Front-end e Designer especialista em criar soluções tecnológicas nos padrões Web<br/>
+      I study English, J'étudie Français, Yo estudio Espanol e falo Português.</p>
+
+      <p>Desenvolvendo sempre soluções com as seguintes tecnologias: CSS, HTML, JavaScript<br/>
+      Webpack, Gulp.js, Grunt.js, ParcelJS, Cypress.io, jQuery, Quasar.js, Nuxt.js, Typescript, ES6<br/>
+      React.js e os Framework Angular, Vue.js e Svelte.js</p>
+
+      <p>Utilizo o Git como versionador de código e o github, bitbucket, gitlab<br/>
+      O pré-processador Stylus para trabalhar o CSS, também trabalho com Less e Sass<br/>
+      Buscando sempre desenvolver e escrever um HTML semântico em sua última versão<br/>
+      Utilizando o máximo que a linguagem oferecer, também utilizo o Pug.js como template</p>
+
+      <p>Meu foco é a melhoria da informação com performance trazendo qualidade e inovação<br/>
+      Criando testes End-to-End usando Cypress.io, gerando resultados positivos nos projetos<br/>
+      Fazendo pesquisas e testes de usabilidade, melhorando a experiência com animações em CSS</p>
+
+      <p>Desenvolvo minhas habilidades artísticas dentro e fora da tecnologia<br/>
+      Escrevendo poesia e também criando desenhos e artes visuais (arte urbana)</p>
+    </div>
+  )
+}
+
+export default App
