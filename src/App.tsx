551702248e160e78336cf70309d74d1fedf21fa4
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-17 21:00:43 -0700

    add suspense and loading route

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,25 @@
-import { useTranslation } from 'react-i18next';
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from "react-router-dom";
 
-import './App.scss'
+import './App.scss';
 
-import { ReactComponent as Linkedin } from './assets/linkedin-logo.svg'
-import { ReactComponent as Duolingo } from './assets/duolingo-logo.svg'
-import { ReactComponent as Codepen } from './assets/codepen-logo.svg'
-import { ReactComponent as Github } from './assets/github-logo.svg'
+import Language from './components/Language';
+import Navigation from './components/Navigation';
+import Loading from './components/Loading';
 
-import Language from './components/Language'
+const Home = lazy(() => import('./views/Home'));
+const Projects = lazy(() => import('./views/Projects'));
 
 function App() {
-  const { t } = useTranslation();
-
   return (
-    <div className='app'>
+    <Suspense fallback={<Loading/>}>
       <Language />
-      <h1 className='name'>Lucas Ferreira de Lima</h1>
-      <p>
-        {t('pageHome.paragraphOne.textOne')}<br/>
-        {t('pageHome.paragraphOne.textTwo')}
-      </p>
-      <p>
-        {t('pageHome.paragraphTwo.textOne')}<br/>
-        {t('pageHome.paragraphTwo.textTwo')}<br/>
-        {t('pageHome.paragraphTwo.textThree')}
-      </p>
-
-      <p>
-        {t('pageHome.paragraphThree.textOne')}<br/>
-        {t('pageHome.paragraphThree.textTwo')}<br/>
-        {t('pageHome.paragraphThree.textThree')}<br/>
-        {t('pageHome.paragraphThree.textFour')}
-      </p>
-
-      <p>
-        {t('pageHome.paragraphFour.textOne')}<br/>
-        {t('pageHome.paragraphFour.textTwo')}<br/>
-        {t('pageHome.paragraphFour.textThree')}
-      </p>
-      
-      <section className='social'>
-        <a href="https://www.linkedin.com/in/lucasferreiralimax" target="_blank">
-          <Linkedin className="icon linkedin" />
-        </a>
-        <a href="https://www.duolingo.com/profile/ferreiralimax" target="_blank">
-          <Duolingo className="icon duolingo" />
-        </a>
-        <a href="https://codepen.io/lucaslimax" target="_blank">
-          <Codepen className="icon codepen" />
-        </a>
-        <a href="https://github.com/lucasferreiralimax" target="_blank">
-          <Github className="icon github" />
-        </a>
-      </section>
-    </div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Suspense>
   )
 }
 
