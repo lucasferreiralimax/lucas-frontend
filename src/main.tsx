134afb2422f94e88eedc63597904d41e73b45d87
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-17 21:01:03 -0700

    add provider BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { I18nextProvider } from 'react-i18next';
+import { BrowserRouter} from "react-router-dom";
 import i18n from './i18n';
 
 import './index.scss'
@@ -9,7 +10,9 @@ import './index.scss'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </I18nextProvider>
   </React.StrictMode>
 )
