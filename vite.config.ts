4864ef9d25a4622984ed458f636a28691b85bd32
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-20 20:08:28 -0300

    addition vite html env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [VitePluginHtmlEnv(), svgr(), react()],
   server: {
     port: 9999
   }
