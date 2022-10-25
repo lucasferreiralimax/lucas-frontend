a55e66d2aa575b4bd2e10550565384f08e527e48
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-25 11:22:27 -0300

    adjustment loading with color primary and className

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -6,12 +6,12 @@ const Loading = () => {
 
   return (
     <div data-testid="app-loading" className="App-loading">
-      <div data-testid="app-loading-loader" className="loader">
-        <svg className="circular" viewBox="25 25 50 50">
-          <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
+      <div data-testid="app-loading-loader" className="app-loading__loader">
+        <svg className="app-loading__circular" viewBox="25 25 50 50">
+          <circle className="app-loading__path" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
         </svg>
       </div>
-      <span>{t('loading')}...</span>
+      <span>{t('loading')}</span>
     </div>
   );
 }
