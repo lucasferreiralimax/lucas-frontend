76bb60787bd6bbf097d0f945b245432c0b6cf43e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-25 12:09:10 -0300

    use quotes simple

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,13 @@
-import i18n from "i18next";
-import backend from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(backend)
   .use(initReactI18next)
   .init({
-    lng: "pt",
-    fallbackLng: "pt",
+    lng: 'pt',
+    fallbackLng: 'pt',
     interpolation: {
       escapeValue: false
     },
