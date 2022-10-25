2f48f3d599b716139d33a5a8dcfb09d883de3d2a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-25 16:12:01 -0300

    test(unit): render text author footer

--- src/components/Footer/test.spec.tsx
+++ src/components/Footer/test.spec.tsx
@@ -1,9 +1,15 @@
+import { describe, expect, it, beforeEach } from 'vitest'
 import { render, screen } from '@testing-library/react';
 import Footer from './index';
 
 describe("Should render <Footer/>", () => {
+  beforeEach(async () => {
+    render(<Footer />);
+  });
   it('renders simple', () => {
-    render(<Footer />)
     expect(screen.getByTestId('app-footer')).toBeInTheDocument()
+  });
+  it('render text 2022 @lucasferreiralimax', () => {
+    expect(screen.getByText('2022 @lucasferreiralimax')).toBeInTheDocument()
   });
 });
