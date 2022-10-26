824e5c5b07b5e812bfccce913a4aa805c9b8778a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-25 21:02:04 -0300

    test(unit): improvement Tech test with loops and show filter

--- src/components/Tech/test.spec.tsx
+++ src/components/Tech/test.spec.tsx
@@ -1,9 +1,45 @@
 import { render, screen } from '@testing-library/react';
+
+import { TechType } from './types';
+
 import Tech from './index';
+import dataTechs from './data';
+
+const techsFilter = ['JavaScript', 'React', 'Vue', 'Angular'];
 
 describe("Should render <Tech/>", () => {
+  beforeEach(() => {
+    render(<Tech />);
+  });
   it('renders simple', () => {
-    render(<Tech />)
-    expect(screen.getByTestId('app-tech')).toBeInTheDocument()
+    expect(screen.getByTestId('app-tech')).toBeInTheDocument();
+  });
+  it('render links tech', () => {
+    for(let tech of dataTechs) {
+      const linkTest = screen.getByTestId(`app-tech-${tech.title}`);
+
+      expect(linkTest).toBeInTheDocument();
+      expect(linkTest.getAttribute('href')).toBe(tech.url);
+      expect(linkTest.getAttribute('class')).toBe(`app-tech__${tech.title}`);
+      expect(linkTest.getAttribute('aria-label')).toBe(`Link ${tech.title}`);
+    }
+  });
+});
+
+describe("Should render <Tech/> with show filter", () => {
+  beforeEach(() => {
+    render(<Tech show={techsFilter} />);
+  });
+  it('render links techsFilter', () => {
+    const mockTechs = dataTechs.filter((item: TechType) => techsFilter.includes(item.title));
+
+    for(let tech of mockTechs) {
+      const linkTest = screen.getByTestId(`app-tech-${tech.title}`);
+
+      expect(linkTest).toBeInTheDocument();
+      expect(linkTest.getAttribute('href')).toBe(tech.url);
+      expect(linkTest.getAttribute('class')).toBe(`app-tech__${tech.title}`);
+      expect(linkTest.getAttribute('aria-label')).toBe(`Link ${tech.title}`);
+    }
   });
 });
