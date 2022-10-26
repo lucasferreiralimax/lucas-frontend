e6e05e09dc4d4a1d799cf509c3ef0925a45c1ecf
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-26 07:39:09 -0300

    test(unit): view Home

---
+++ src/views/Home/test.spec.tsx
@@ -0,0 +1,44 @@
+import { describe, expect, it, beforeEach } from 'vitest'
+import { render, screen } from '@testing-library/react';
+import Home from './index';
+
+describe('Should render <Home/>', () => {
+  beforeEach(() => {
+    render(<Home />);
+  });
+  it('renders simple', () => {
+    expect(screen.getByTestId('app-home')).toBeInTheDocument();
+  });
+  it('render title document', () => {
+    expect(document.title).toBe('Lucas Front-end');
+  });
+  it('render photo-profile', () => {
+    const photo = screen.getByAltText('Lucas Ferreira de Lima');
+    expect(photo).toBeInTheDocument();
+    expect(photo).toHaveAttribute('src', '/src/assets/lucaslucas.jpg');
+    expect(photo).toHaveAttribute('class', 'app-profile__photo');
+    expect(photo).toHaveAttribute('width', '70%');
+  });
+  it('render translates paragraphs', () => {
+    // paragraphOne
+    expect(screen.getByText(/pageHome.paragraphOne.textOne/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphOne.textTwo/i)).toBeInTheDocument();
+
+    // paragraphTwo
+    expect(screen.getByText(/pageHome.paragraphTwo.textOne/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphTwo.textTwo/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphTwo.textThree/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphTwo.textFour/i)).toBeInTheDocument();
+
+    // paragraphThree
+    expect(screen.getByText(/pageHome.paragraphThree.textOne/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphThree.textTwo/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphThree.textThree/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphThree.textFour/i)).toBeInTheDocument();
+
+    // paragraphFour
+    expect(screen.getByText(/pageHome.paragraphFour.textOne/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphFour.textTwo/i)).toBeInTheDocument();
+    expect(screen.getByText(/pageHome.paragraphFour.textThree/i)).toBeInTheDocument();
+  });
+});
