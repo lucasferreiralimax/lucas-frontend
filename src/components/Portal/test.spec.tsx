ca269460a11d928b8c9c7810e52649c82601ca97
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-26 08:08:21 -0300

    test(unit): improvement validation portal with child

--- src/components/Portal/test.spec.tsx
+++ src/components/Portal/test.spec.tsx
@@ -13,3 +13,19 @@ describe('Should render <Portal/>', () => {
     expect(screen.getByTestId('app-portal')).toHaveAttribute('class', 'root-portal');
   });
 });
+
+describe('Should render <Portal/> with child', () => {
+  beforeEach(() => {
+    render(
+      <Portal>
+        <h1>Test Portal Child</h1>
+      </Portal>
+    );
+  });
+  it('renders simple Portal', () => {
+    expect(screen.getByTestId('app-portal')).toBeInTheDocument();
+  });
+  it('renders validation have child', () => {
+    expect(screen.getByText('Test Portal Child')).toBeInTheDocument();
+  });
+});
