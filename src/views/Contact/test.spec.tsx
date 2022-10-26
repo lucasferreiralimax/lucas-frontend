a46e7c52d7e89dc7090c27d09c6c15e79c4eb8bb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-26 08:01:26 -0300

    test(unit): improvement validation contact list

--- src/views/Contact/test.spec.tsx
+++ src/views/Contact/test.spec.tsx
@@ -1,15 +1,30 @@
 import { describe, expect, it, beforeEach } from 'vitest'
 import { render, screen } from '@testing-library/react';
+
 import Contact from './index';
+import contacts from './data';
 
 describe('Should render <Contact/>', () => {
   beforeEach(() => {
     render(<Contact />);
   });
-  it('renders simple', () => {
+  it('renders simple and title', () => {
     expect(screen.getByTestId('app-social')).toBeInTheDocument();
+    expect(screen.getByText('🌎 pageContact.title')).toBeInTheDocument();
   });
   it('render title document', () => {
     expect(document.title).toBe('Lucas navigation.contact');
+  });
+  it('render list contacts', () => {
+
+    contacts.forEach(contact => {
+      const linkRegex = new RegExp(`Link ${contact.name}`, 'i');
+      const linkTest = screen.getAllByRole('link', { name: linkRegex });
+
+      linkTest.forEach(link => {
+        expect(link).toBeInTheDocument();
+        expect(link.getAttribute('href')).toBe(contact.link);
+      });
+    });
   });
 });
