8420f7ae0a10558be23b14b4f2f77aa1c6c03d01
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-03-14 23:53:55 -0300

    test(unit): adjustment specs of projects

--- src/views/Projects/test.spec.tsx
+++ src/views/Projects/test.spec.tsx
@@ -4,7 +4,6 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from '@/i18n';
 
 import Projects from './index';
-
 import projects from './data';
 
 describe('Should render <Projects/>', () => {
