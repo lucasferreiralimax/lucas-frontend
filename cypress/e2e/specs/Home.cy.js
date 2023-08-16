22561da1e63f0a39cabb13eaa2735c9b8e69c9e8
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-08-16 19:34:08 -0300

    test(e2e): update specs of header remove flag

--- cypress/e2e/specs/Home.cy.js
+++ cypress/e2e/specs/Home.cy.js
@@ -4,7 +4,6 @@ describe('Test render home page', () => {
   it('Render header with Brasil, Lucas and link /', () => {
     cy.visit('/')
     cy.get('.app-header__name a').invoke('attr', 'href').should('contain', '/')
-    cy.get('[aria-label="Brasil"]').should('exist')
     cy.contains('.app-header__name', 'Lucas Ferreira de Lima')
   })
   it('Render photo profile', () => {
