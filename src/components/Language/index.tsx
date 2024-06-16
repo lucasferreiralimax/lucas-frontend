418bf330bb223a48a6ac97141a745dbdbc93cdd7
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-16 12:40:21 -0300

    change language select for flag button

--- src/components/Language/index.tsx
+++ src/components/Language/index.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import './style.scss';
 
 const Language = () => {
   const { t, i18n } = useTranslation();
+  const [current, setCurrent] = useState(localStorage.getItem('language') || 'pt');
+  const languages = ['pt-BR', 'en-US', 'es-ES', 'ru-RU', 'zh-CN'];
 
   useEffect(() => {
     const language = localStorage.getItem('language');
@@ -18,23 +20,23 @@ const Language = () => {
     i18n.changeLanguage(value);
     localStorage.setItem('language', value);
     document.documentElement.lang = value;
+    setCurrent(value)
   }
 
   return (
-    <>
+    <section className='app-language'>
       <h4 className='app-language__title'>{t('languages.title')}</h4>
-      <select
-        name='language'
-        className='app-language'
-        data-testid='app-language'
-        value={i18n.language}
-        onChange={({ target: { value }}) => changeLanguages(value)}
-      >
-        <option value='pt'>{t('languages.pt')}</option>
-        <option value='en'>{t('languages.en')}</option>
-        <option value='es'>{t('languages.es')}</option>
-      </select>
-    </>
+      {languages.map((language) => (
+        <button
+          key={language}
+          onClick={() => changeLanguages(language.slice(0, 2))}
+          className={`app-language-flag ${current === language.slice(0, 2) && 'active'}`}
+        >
+          <img width="30px" src={`./src/assets/flags/${language}.svg`} alt={language} />
+          {language}
+        </button>
+      ))}
+    </section>
   )
 }
 
