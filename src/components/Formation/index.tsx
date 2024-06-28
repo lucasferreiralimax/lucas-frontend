d67753fb77511df6bc55c2e050626ea77700e536
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-28 10:10:43 -0300

    adjustment link in formation

--- src/components/Formation/index.tsx
+++ src/components/Formation/index.tsx
@@ -1,46 +1,51 @@
-import { useTranslation } from 'react-i18next';
-import LinkIcon from '@/assets/icons/link-icon.svg?react';
+import { useTranslation } from "react-i18next";
+import LinkIcon from "@/assets/icons/link-icon.svg?react";
 
-import { FormationProps } from './types';
+import { FormationProps } from "./types";
 
-import './style.scss';
+import "./style.scss";
 
 const Formation = ({ dataTestid, data }: FormationProps) => {
   const { t } = useTranslation();
 
   return (
-    <div data-testid={dataTestid} className='app-formation'>
-      <h3 className='app-formation__title'>{t(`${data.translate}.title`)}</h3>
-      <div className='app-formation__detail'>
-        <a
-          href={data.url}
-          target='_blank'
-          role="link"
-          aria-label={`Link ${t(`${data.translate}.title`)}`}
-        >
-          <img width='70' src={data.photo} alt={t(`${data.translate}.title`)} />
-        </a>
-        <div className='app-formation__description'>
-          {data.date.start} - {data.date.end}<br/>
+    <div data-testid={dataTestid} className="app-formation">
+      <h3 className="app-formation__title">{t(`${data.translate}.title`)}</h3>
+      <div className="app-formation__detail">
+        <div className="app-formation__header">
+          <a
+            href={data.url}
+            target="_blank"
+            role="link"
+            aria-label={`Link ${t(`${data.translate}.title`)}`}
+          >
+            <img
+              width="70"
+              src={data.photo}
+              alt={t(`${data.translate}.title`)}
+            />
+          </a>
+          {data.url && (
+            <a
+              href={data.url}
+              target="_blank"
+              role="link"
+              aria-label={`Link ${t(`${data.translate}.title`)}`}
+            >
+              <LinkIcon className="app-formation__icon link" />
+              <span>Link</span>
+            </a>
+          )}
+        </div>
+        <div className="app-formation__description">
+          {data.date.start} - {data.date.end}
+          <br />
           <hr></hr>
           {t(`${data.translate}.description`)}
         </div>
       </div>
-      {data.url && (
-        <div className='app-formation__actions'>
-          <a
-            href={data.url}
-            target='_blank'
-            role="link"
-            aria-label={`Link ${t(`${data.translate}.title`)}`}
-          >
-            <LinkIcon className='app-formation__icon link' />
-            <span>Link</span>
-          </a>
-        </div>
-      )}
     </div>
   );
-}
+};
 
 export default Formation;
